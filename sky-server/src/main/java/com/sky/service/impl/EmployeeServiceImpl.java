d26304f0f8ae240bbda0a90ada9ba03238623f13
ding <2795876032@qq.com>
2025-12-02 16:30:07 +0800

    1

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -40,6 +40,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //密码比对
         // TODO 后期需要进行md5加密，然后再进行比对
+        //对前端传过来的密码进行md5加密处理
+        password = DigestUtils.md5DigestAsHex(password.getBytes());//进行md5处理
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
